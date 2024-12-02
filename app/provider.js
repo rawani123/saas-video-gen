1495dc64883dbd89f50a3f1b9465a144189bfae1
rawani123 <rawoolaniruddha3@gmail.com>
2024-12-02 12:01:13 +0530

    done

---
+++ app/provider.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Provider = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Provider;
---
+++ config/schema.js
@@ -0,0 +1,10 @@
+import { boolean, serial, varchar } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const Users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull(),
+  imageUrl: varchar('image_url', { length: 255 }),
+  subscription: boolean('subscription').default(false),
+});
