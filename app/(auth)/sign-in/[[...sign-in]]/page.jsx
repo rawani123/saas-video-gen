baff5a35534c988ea17b4022667875e70a474a49
rawani123 <rawoolaniruddha3@gmail.com>
2024-12-02 20:46:12 +0530

    feat: clerk and custom sign,signup added

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,17 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div>
+          <Image className="w-full object-contain" src={"/image.png"} alt="login" width={800} height={900} />
+        </div>
+        <div className="flex justify-center items-center h-screen">
+          <SignIn />
+        </div>
+      </div>
+    </>
+  );
+}
--- app/provider.js
+++ app/provider.js
@@ -1,7 +1,38 @@
-import React from "react";
+"use client"
+
+import { db } from "@/config/db";
+import { Users } from "@/config/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
 
 const Provider = ({ children }) => {
-  return <div>{children}</div>;
+  const { user } = useUser();
+
+  useEffect(() => {
+    const upsertUser = async () => {
+      if (!user) return;
+
+      const email = user.primaryEmailAddress?.emailAddress;
+
+      const [existingUser] = await db
+        .select()
+        .from(Users)
+        .where(eq(Users.email, email));
+
+      if (!existingUser) {
+        await db.insert(Users).values({
+          name: user.fullName,
+          email,
+          imageUrl: user.imageUrl,
+        });
+      }
+    };
+
+    upsertUser();
+  }, [user]);
+
+  return <>{children}</>;
 };
 
 export default Provider;
