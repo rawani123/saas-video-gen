9cac76af00faada2b689f729c5c5d89c3896fb5c
rawani123 <rawoolaniruddha3@gmail.com>
2024-12-23 12:25:00 +0530

    fix

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -4,22 +4,6 @@ import Header from "./_components/Header";
 import SideNav from "./_components/SideNav";
 import { VideoDataContext } from "../_context/VideoDataContext";
 
-export const metadata = {
-  title: "MediaForge Dashboard",
-  description:
-    "Access and manage your projects, media, and tools effortlessly in the MediaForge Dashboard. Streamline your workflow with an intuitive interface and advanced features.",
-  keywords: [
-    "MediaForge Dashboard",
-    "media management",
-    "video projects",
-    "photo editing",
-    "dashboard interface",
-    "media production tools",
-  ],
-  author: "MediaForge Team",
-  viewport: "width=device-width, initial-scale=1.0",
-  charset: "UTF-8",
-};
 
 const dashboardLayout = ({ children }) => {
   const [videoData, setVideoData] = useState([]);
