03e5508ae0e07961ef6536fe2ce2e47f7ded38eb
rawani123 <rawoolaniruddha3@gmail.com>
2024-12-23 12:23:42 +0530

    fix:metaData

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,8 @@
-
-import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Provider from "./provider";
-import {Outfit} from "next/font/google"
+import { Outfit } from "next/font/google";
 
-
-const outfit = Outfit({subsets:["latin"]})
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "MediaForge - Effortless Media Production",
@@ -22,18 +19,21 @@ export const metadata = {
     "high-quality media",
   ],
   author: "MediaForge Team",
-  viewport: "width=device-width, initial-scale=1.0",
   charset: "UTF-8",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: "no",
+};
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={outfit.className}
-        >
+        <body className={outfit.className}>
           <Provider>{children}</Provider>
         </body>
       </html>
